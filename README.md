9c803c7cb482fe24953c3593f60f858d6918bb94
ramilmavlihanov <Ramil040201@gmail.com>
2024-08-06 22:04:08 +0300

    ADD: new HomeWork

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ main - break points
 module_2.2 - is everyone equal?
 module_2.3 - Zeros are nothing, negation is unacceptable
 module_2.4 - It's not that simple
+module_2.5 - Matrix
---
+++ module_2.5.py
@@ -0,0 +1,21 @@
+def get_matrix(m=3, n=4, value="val"):
+    matrix = []
+    if isinstance(m, int) and isinstance(n, int):
+        for columns in range(m):
+            row = []
+            for rows in range(n):
+                row.append(value)
+            matrix.append(row)
+    return matrix
+
+
+columns = input("Enter the number of columns: ")
+rows = input("Enter the number of rows: ")
+value = input("Enter the value: ")
+
+if columns.isdigit() and rows.isdigit():
+    matrix = get_matrix(int(rows), int(columns), value)
+    for col in range(int(rows)):
+        print(matrix[col])
+else:
+    print("Invalid data")
